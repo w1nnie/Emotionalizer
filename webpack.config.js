c220a9634ce55907d5f1893405bfac0d768209b7
w1nnie <winnie.trap@gmail.com>
2022-07-17 18:00:04 +0900

    Live2D開発環境とtextalive開発環境を統合

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,47 @@ const path = require('path')
 
 module.exports = {
     mode:'development',
+    target: ['web', 'es5'],
     entry: {
-        index: './src/index.js',
+        textalive: './src/textalive/index.js',
+        Live2D: './src/Live2D/main.ts',
     },
     output: {
-        filename: '[name].js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: './dist/'
+    },
+    resolve: {
+        extensions: ['.ts','.js'],
+        alias: {
+            '@framework': path.resolve(__dirname, './src/CubismSdk/Framework/src'),
+            '@core': path.resolve(__dirname, './src/CubismSdk/Core')
+        }
     },
     cache: true,
     devServer: {
         static: {
-            directory: path.join(__dirname, "dist"),
+            directory: path.join(__dirname, "/"),
         },
         port: 8080,
         open: true,
         watchFiles: ['src/**/*', 'dist/**/*'],
         compress: true,
+        hot: true,
+        devMiddleware: {
+            writeToDisk: true,
+        },
     },
     module: {
         rules: [
             {
                 test: /\.txt/,
                 type: 'asset/source',
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
             }
         ]
     }
